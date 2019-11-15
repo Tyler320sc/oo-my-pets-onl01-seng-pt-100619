6c5b153beeeace4e6f758bc9891b70e13e45d8c8
Tyler Carroll <tylercarroll1611@gmail.com>
2019-11-15 01:30:12 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -4,3 +4,18 @@ end
 
 
 
+# attr_accessor :mood, :owner
+#   attr_reader :name
+
+#   @@all = []
+
+#   def initialize(name, owner)
+#     @name = name
+#     @mood = "nervous"
+#     @owner = owner
+#     @@all << self
+#   end
+
+#   def self.all
+#     @@all
+#   end
