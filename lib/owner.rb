447ae00be898d767c1e60a47d52bbeca9f4cea05
Tyler Carroll <tylercarroll1611@gmail.com>
2019-11-15 01:19:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -9,10 +9,42 @@ class Owner
     @@all << self
   end 
   
-   def say_species
+  def say_species
     "I am a #{self.species}."
   end
   
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
   
   
   end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
