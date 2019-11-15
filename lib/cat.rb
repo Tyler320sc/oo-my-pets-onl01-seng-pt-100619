c86eac1c78728cc64356f06233ad1911d67feb4b
Tyler Carroll <tylercarroll1611@gmail.com>
2019-11-15 01:36:15 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,4 +1,18 @@
 class Cat
+  attr_accessor :mood, :owner
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name 
+    @mood = nervous
+    @owner = owner
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all 
+  end 
   
 end
 
